689731179509265dad8d18c990569cb5a8de35ef
zhangxiaoguang.93 <zhangxiaoguang.93@bytedance.com>
2025-08-05 19:35:10 +0800

    feat(keymaps): 添加bash风格键位映射和窗口切换功能
    添加Insert模式下的bash风格键位映射，包括光标移动和删除操作
    新增窗口切换功能，并设置jk快速退出Insert模式并保存文件
    设置全局leader键为逗号和反斜杠

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,31 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Insert模式下的bash风格键位映射
+map("i", "<C-a>", "<ESC>^i")
+map("i", "<C-e>", "<End>")
+map("i", "<C-h>", "<BS>")
+map("i", "<C-b>", "<Left>")
+map("i", "<C-f>", "<Right>")
+map("i", "<C-d>", "<Del>")
+map("i", "<C-o>", "<C-\\><C-o>O")
+map("n", "<ESC>", "<cmd>noh<CR>")
+
+-- 窗口切换（仅在非VSCode环境下）
+-- 基础键位设置
+local opts = {
+    noremap = true,
+    silent = true
+}
+
+map("i", "jj", "<ESC>", opts)
+-- Insert模式下使用jk���并保存
+map("i", "jk", "<ESC>:w<CR>", opts)
+
+
+-- 保存文件
+map("n", "<D-s>", ":w<CR>", opts)
+map("i", "<D-s>", "<Esc>:w<CR>", opts)
