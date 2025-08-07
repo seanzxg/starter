fa75d453d4d2c085f39bfa14fa26167136a6fee8
zhangxiaoguang.93 <zhangxiaoguang.93@bytedance.com>
2025-08-07 11:54:08 +0800

    feat: change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,63 +1,77 @@
 return { -- add gruvbox
-{"ellisonleao/gruvbox.nvim"}, -- Configure LazyVim to load gruvbox
-{
-    "LazyVim/LazyVim",
-    opts = {
-        colorscheme = "gruvbox"
-    }
-}, {
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+  {
     "folke/snacks.nvim",
-    ---@type snacks.Config
     opts = {
-        dashboard = {
-            formats = {
-                key = function(item)
-                    return {{
-                        "[",
-                        hl = "special"
-                    }, {
-                        item.key,
-                        hl = "key"
-                    }, {
-                        "]",
-                        hl = "special"
-                    }}
-                end
-            },
-            sections = {{
-                section = "terminal",
-                cmd = "fortune -s | cowsay",
-                hl = "header",
-                padding = 1,
-                indent = 8
-            }, {
-                title = "MRU",
-                padding = 1
-            }, {
-                section = "recent_files",
-                limit = 8,
-                padding = 1
-            }, {
-                title = "MRU ",
-                file = vim.fn.fnamemodify(".", ":~"),
-                padding = 1
-            }, {
-                section = "recent_files",
-                cwd = true,
-                limit = 8,
-                padding = 1
-            }, {
-                title = "Sessions",
-                padding = 1
-            }, {
-                section = "projects",
-                padding = 1
-            }, {
-                title = "Bookmarks",
-                padding = 1
-            }, {
-                section = "keys"
-            }}
-        }
-    }
-}, }
+      dashboard = {
+        formats = {
+          key = function(item)
+            return {
+              {
+                "[",
+                hl = "special",
+              },
+              {
+                item.key,
+                hl = "key",
+              },
+              {
+                "]",
+                hl = "special",
+              },
+            }
+          end,
+        },
+        sections = {
+          {
+            section = "terminal",
+            cmd = "fortune -s | cowsay",
+            hl = "header",
+            padding = 1,
+            indent = 8,
+          },
+          {
+            title = "MRU",
+            padding = 1,
+          },
+          {
+            section = "recent_files",
+            limit = 8,
+            padding = 1,
+          },
+          {
+            title = "MRU ",
+            file = vim.fn.fnamemodify(".", ":~"),
+            padding = 1,
+          },
+          {
+            section = "recent_files",
+            cwd = true,
+            limit = 8,
+            padding = 1,
+          },
+          {
+            title = "Sessions",
+            padding = 1,
+          },
+          {
+            section = "projects",
+            padding = 1,
+          },
+          {
+            title = "Bookmarks",
+            padding = 1,
+          },
+          {
+            section = "keys",
+          },
+        },
+      },
+    },
+  },
+}
