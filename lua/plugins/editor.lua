b487bec36ee6a1a1247be8748a57af0c124e282e
zhangxiaoguang.93 <zhangxiaoguang.93@bytedance.com>
2025-08-21 20:03:13 +0800

    refactor(editor): 优化插件配置并添加mini.surround映射
    - 调整flash.nvim的配置结构，使其更清晰易读
    - 禁用默认的s/S/t/T键位映射以避免冲突
    - 添加mini.surround插件及其键位映射配置

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,63 +1,88 @@
-return {{
+return {
+  {
     "ggandor/flit.nvim",
-    enabled = false
-}, {
+    enabled = false,
+  },
+  {
     "ggandor/leap.nvim",
-    enabled = false
-}, {
+    enabled = false,
+  },
+  {
     "folke/flash.nvim",
     opts = {
-        modes = {
-            char = {
-                enabled = false -- 禁用默认的 char 模式，这通常使用 s 键
-            }
+      modes = {
+        char = {
+          enabled = false, -- 禁用默认的 char 模式，这通常使用 s 键
         },
-        label = {
-            uppercase = false
-        },
+      },
+      label = {
+        uppercase = false,
+      },
     },
     keys = { -- 明确禁用默认的 s 键
-    {"s", false}, {"S", false}, -- 设置我们想要的键位映射
-    {"t", false}, {"T", false}, -- 设置我们想要的键位映射
-    {
+      { "s", false },
+      { "S", false }, -- 设置我们想要的键位映射
+      { "t", false },
+      { "T", false }, -- 设置我们想要的键位映射
+      {
         "f",
-        mode = {"n", "x", "o"},
+        mode = { "n", "x", "o" },
         function()
-            require("flash").jump({
-               search = { forward = true, wrap = false, multi_window = false },
-            })
+          require("flash").jump({
+            search = { forward = true, wrap = false, multi_window = false },
+          })
         end,
-        desc = "Flash Jump"
-    },
-    {
+        desc = "Flash Jump",
+      },
+      {
         "t",
-        mode = {"n", "x", "o"},
+        mode = { "n", "x", "o" },
         function()
-            require("flash").jump({
-                search = { forward = false, wrap = false, multi_window = false },
-            })
+          require("flash").jump({
+            search = { forward = false, wrap = false, multi_window = false },
+          })
         end,
-        desc = "Flash Jump"
-    }, {
+        desc = "Flash Jump",
+      },
+      {
         "F",
-        mode = {"n", "o", "x"},
+        mode = { "n", "o", "x" },
         function()
-            require("flash").treesitter()
+          require("flash").treesitter()
         end,
-        desc = "Flash Treesitter"
-    }, {
+        desc = "Flash Treesitter",
+      },
+      {
         "T",
-        mode = {"o", "x"},
+        mode = { "o", "x" },
         function()
-            require("flash").treesitter_search()
+          require("flash").treesitter_search()
         end,
-        desc = "Treesitter Search"
-    }, {
+        desc = "Treesitter Search",
+      },
+      {
         "<c-s>",
-        mode = {"c"},
+        mode = { "c" },
         function()
-            require("flash").toggle()
+          require("flash").toggle()
         end,
-        desc = "Toggle Flash Search"
-    }}
-}}
+        desc = "Toggle Flash Search",
+      },
+    },
+  },
+  {
+
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "sa", -- Add surrounding in Normal and Visual modes
+        delete = "sd", -- Delete surrounding
+        find = "sf", -- Find surrounding (to the right)
+        find_left = "sF", -- Find surrounding (to the left)
+        highlight = "sh", -- Highlight surrounding
+        replace = "sr", -- Replace surrounding
+        update_n_lines = "sn", -- Update `n_lines`
+      },
+    },
+  },
+}
