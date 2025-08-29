35adfa2c2f7bf0610869afe8c7c111d49c7ee867
zhangxiaoguang.93 <zhangxiaoguang.93@bytedance.com>
2025-08-29 17:27:02 +0800

    refactor(buffer): 移除注释掉的bufferline插件配置
    清理不再使用的bufferline插件配置，保持配置文件的简洁性

--- lua/plugins/buffer.lua
+++ lua/plugins/buffer.lua
@@ -1,8 +1,10 @@
-return {{
-	"akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "M", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-      { "m", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    }
-}}
+return {
+--   {
+-- 	"akinsho/bufferline.nvim",
+--     event = "VeryLazy",
+--     keys = {
+--       { "M", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+--       { "m", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+--     }
+-- }
+}
